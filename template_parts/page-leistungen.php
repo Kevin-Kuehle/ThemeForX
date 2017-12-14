5bfc6c91b4ea1a784f9e944a60e78764fa23682e
Kevin Kühle <Kevin.kuehle@googlemail.com>
2017-12-14 15:55:53 +0100

    ertert

--- template_parts/page-leistungen.php
+++ template_parts/page-leistungen.php
@@ -4,76 +4,77 @@
 */ ?>
 <?php get_header(); ?>
 <main>
-	<div class="pageTitle">
-		<div class="wrap">
-			<div class="text"><h1>Leistungen <br> <span>von Kühle-Webdesign</span></h1></div>
-			<div class="image"><img src=" <?php echo get_template_directory_uri() . '/assets/images/webdevelopment.png' ?> " alt="Leistungen von Kuehle-Webdesign"></div>
-		</div>
-	</div>
-	<div class="row">
-		<h2><b>Das ist in jedem Paket enthalten</b> bei Kühle-Webdesign:</h2>
-		<ul class="leistungen listStyle-2">
-			<li>Professionelle Betreuung</li>
-			<li>Design Besprechung</li>
-			<li>Design Erstellung</li>
-			<li>Responsive Design</li>
-			<li>CMS Wordpress</li>
-			<li>SSL Zertifikat</li>
-			<li>SEO (Suchmaschinenoptimierung)</li>
-			<li>Lizenzierte Bilder von ShutterStock</li>
-			<li>Server & Domain Hosting</li>
-			<li>Design Umsetzung nach Abnahme</li>
-			<li>Enge Zusammenarbeit bis zum Abschluss</li>
-		</ul>
-	</div>
-	<div class="row">
-		<h2>Packet Unterscheidungen</h2>
+    <div class="pageTitle">
+        <div class="wrap">
+            <div class="text"><h1>Leistungen <br> <span>von Kühle-Webdesign</span></h1></div>
+            <div class="image"><img src=" <?php echo get_template_directory_uri() . '/assets/images/webdevelopment.png' ?> " alt="Leistungen von Kuehle-Webdesign"></div>
+        </div>
+    </div>
+    <div class="row">
+        <h2><b>Das ist in jedem Paket enthalten</b> bei Kühle-Webdesign:</h2>
+        <ul class="leistungen listStyle-2">
+            <li>Professionelle Betreuung</li>
+            <li>Design Besprechung</li>
+            <li>Design Erstellung</li>
+            <li>Responsive Design</li>
+            <li>CMS Wordpress</li>
+            <li>SSL Zertifikat</li>
+            <li>SEO (Suchmaschinenoptimierung)</li>
+            <li>Lizenzierte Bilder von ShutterStock</li>
+            <li>Server & Domain Hosting</li>
+            <li>Design Umsetzung nach Abnahme</li>
+            <li>Enge Zusammenarbeit bis zum Abschluss</li>
+        </ul>
+    </div>
+    <div class="row overflowX">
+        <table class="table packetLeistungen" width="100" cellpadding="0" cellspacing="0" style="border-spacing: 10px 0;">
+            <!-- Header of the Table -->
+            <tr>
+                <td><h2>Packet Unterscheidungen</h2></td>
+                <td>
+                    <object class="logo" width="75" data="<?php echo get_template_directory_uri() . '/assets/images/iconStarter.svg' ?>" type="image/svg+xml">Starter</object></td>
+                <td>
+                    <object class="logo" width="75" data="<?php echo get_template_directory_uri() . '/assets/images/iconBasic.svg' ?>" type="image/svg+xml">Basic</object></td>
+                <td>
+                    <object class="logo" width="75" data="<?php echo get_template_directory_uri() . '/assets/images/iconBusiness.svg' ?>" type="image/svg+xml">Business</object></td>
+            </tr>
 
-		<table class="packetLeistungen" width="100">
-			<!-- Header of the Table -->
-			<tr>
-				<td><h2>Packet Unterscheidungen</h2></td>
-				<td></td>
-				<td></td>
-				<td></td>
-			</tr>
+            <tr class="title">
+                <th class="title">Leistungen</th>
+                <th class="starter">Starter</th>
+                <th class="basic">Basic</th>
+                <th class="business">Business</th>
+            </tr>
 
-			<tr class="title">
-				<th>Leistungen</th>
-				<th>Starter</th>
-				<th>Basic</th>
-				<th>Business</th>
-			</tr>
-			
-			<tr>
-				<td>Stunden für die Design Phase (maximal)</td>
-				<td>24</td>
-				<td>48</td>
-				<td>60</td>
-			</tr>
+            <tr>
+                <td>Stunden für die Design Phase (maximal)</td>
+                <td class="starter">24</td>
+                <td class="basic">11</td>
+                <td class="business">60</td>
+            </tr>
 
-			<tr>
-				<td>Stunden für die Umsetzung (Wordpress, Php, Javascript, CSS3)</td>
-				<td>23</td>
-				<td>11</td>
-				<td>44</td>
-			</tr>
+            <tr>
+                <td>Stunden für die Umsetzung (Wordpress, Php, Javascript, CSS3)</td>
+                <td class="starter">23</td>
+                <td class="basic">11</td>
+                <td class="business">44</td>
+            </tr>
 
-			<tr>
-				<td>Suchmachinen Optimierung Analyse & Stundeninvestition</td>
-				<td>224</td>
-				<td>12</td>
-				<td>43</td>
-			</tr>
+            <tr>
+                <td>Suchmachinen Optimierung Analyse & Stundeninvestition</td>
+                <td class="starter">224</td>
+                <td class="basic">12</td>
+                <td class="business">43</td>
+            </tr>
 
-			<tr>
-				<td>Lizensierte Bilder von ShutterStock</td>
-				<td>3</td>
-				<td>6</td>
-				<td>12</td>
-			</tr>
-		</table>
-	</div>
+            <tr>
+                <td>Lizensierte Bilder von ShutterStock</td>
+                <td class="starter">3</td>
+                <td class="basic">6</td>
+                <td class="business">12</td>
+            </tr>
+        </table>
+    </div>
 
 </main>
 <?php get_footer(); ?>
